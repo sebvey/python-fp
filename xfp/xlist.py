778968db8596c46b8c0a4208800ba3d946aab02c
jliermann <liermannjerome@gmail.com>
2024-05-27 16:14:26 +0200

    feat: add folds on Xlist

--- xfp/xlist.py
+++ xfp/xlist.py
@@ -32,6 +32,19 @@ class Xlist[T: E]:
             case _:
                 return False
 
+    def __len__(self) -> int:
+        return len(self.__data)
+
+    def head(self) -> T:
+        if len(self) <= 0:
+            raise IndexError("<head> operation not allowed on empty list")
+        return self.__data[0]
+
+    def tail(self) -> "Xlist[T]":
+        if len(self) <= 0:
+            raise IndexError("<tail> operation not allowed on empty list")
+        return Xlist(self.__data[1:])
+
     def map(self, f: Callable[[T], E]) -> "Xlist[E]":
         return Xlist([f(el) for el in self])
 
@@ -39,8 +52,7 @@ class Xlist[T: E]:
         return Xlist([el for el in self if predicate(el)])
 
     def foreach(self, statement: Callable[[T], None]) -> None:
-        for el in self:
-            statement(el)
+        (statement(e) for e in self)
 
     def flatten(self) -> "Xlist[E]":
         return Xlist([inner for e in self if isinstance(e, ABCIterable) for inner in e])
@@ -56,3 +68,34 @@ class Xlist[T: E]:
 
     def sorted(self, key: Callable[[T], E] = _id, reverse: bool = False) -> "Xlist[T]":
         return Xlist(sorted(self, key=key, reverse=reverse))
+
+    def reverse(self) -> "Xlist[T]":
+        data: list[T] = self.__data.copy()
+        data.reverse()
+        return Xlist(data)
+
+    def foldLeft(self, zero: E) -> Callable[[Callable[[E, T], E]], E]:
+        def inner(f: Callable[[E, T], E]) -> E:
+            acc: E = zero
+            for e in self:
+                acc = f(acc, e)
+            return acc
+
+        return inner
+
+    def foldRight(self, zero: E) -> Callable[[Callable[[T, E], E]], E]:
+        def inner(f: Callable[[T, E], E]) -> E:
+            return self.reverse().foldLeft(zero)(lambda e, t: f(t, e))
+
+        return inner
+
+    def fold(self, zero: T) -> Callable[[Callable[[T, T], E]], E]:
+        def inner(f: Callable[[T, T], E]) -> E:
+            return self.foldLeft(zero)(f)
+
+        return inner
+
+    def reduce(self, f: Callable[[T, T], T]) -> T:
+        if len(self) <= 0:
+            raise IndexError("<reduce> operation not allowed on empty list")
+        return self.tail().fold(self.head())(f)
