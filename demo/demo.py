23854136288b407c35d96b49fae139c110f7b819
jeliermann <jliermann@groupeseb.com>
2024-06-07 15:06:56 +0200

    feat: some utils function + combined demo

---
+++ demo/demo.py
@@ -0,0 +1,30 @@
+import functools
+from xfp import Xlist, Xeffect, XFXBranch
+from xfp.utils import tupled
+
+print("@@@ EXPLORE Object @@@")
+
+
+def f(a, b):
+    return a + b
+
+
+print("@@@ PYTHONIC STYLE @@@")
+
+for g in dir(f):
+    try:
+        print((getattr(f, g)(), g))
+    except Exception:
+        pass
+
+
+print("@@@ FUNCTIONAL STYLE @@@")
+
+(
+    Xlist(dir(f))
+    .map(functools.partial(getattr, f))
+    .map(Xeffect.from_unsafe)
+    .zip(dir(f))
+    .filter(tupled(lambda effect, _: effect.branch == XFXBranch.LEFT))
+    .foreach(print)
+)
--- demo/xlist/operation.py
+++ demo/xlist/operation.py
@@ -1,4 +1,4 @@
-from domain import Sale
+from .domain import Sale
 from dataclasses import replace
 
 
