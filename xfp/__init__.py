d8340bfb7df7fcebca75c6b6b838eb16f280bf5d
jeliermann <jliermann@ippon.fr>
2024-09-18 14:36:24 +0200

    feat: expose utils

--- xfp/__init__.py
+++ xfp/__init__.py
@@ -1,5 +1,6 @@
 from xfp.xiter import Xiter
 from xfp.xeffect import Xeffect, XFXBranch
 from xfp.xlist import Xlist
+from xfp.utils import curry, curry_method, tupled
 
-__all__ = ["Xiter", "Xlist", "Xeffect", "XFXBranch"]
+__all__ = ["Xiter", "Xlist", "Xeffect", "XFXBranch", "curry", "curry_method", "tupled"]
