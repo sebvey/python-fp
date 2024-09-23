8dd36d8a4e94021d0f0f683e0b88938ac5e7dd02
jeliermann <jliermann@ippon.fr>
2024-09-23 10:48:35 +0200

    fix: circular import problem

--- xfp/__init__.py
+++ xfp/__init__.py
@@ -1,6 +1,7 @@
-from xfp.xiter import Xiter
+# Warning : import order matters
 from xfp.xeffect import Xeffect, XFXBranch
 from xfp.xlist import Xlist
+from xfp.xiter import Xiter
 from xfp.utils import curry, curry_method, tupled
 
 __all__ = ["Xiter", "Xlist", "Xeffect", "XFXBranch", "curry", "curry_method", "tupled"]
