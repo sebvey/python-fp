baafe75683dc9d5ef96f5afe90cb4e744f56cd02
jeliermann <107553762+jeliermann@users.noreply.github.com>
2024-09-27 09:26:29 +0200

    recover, recover_with, filter (#8)
    * feat: add recover_with method
    
    * feat: add recover methods
    
    - homogenize bias usage to do so also
    
    * feat: add filter methods
    
    * improve: add doc

--- demo/xeffect/operation.py
+++ demo/xeffect/operation.py
@@ -3,4 +3,4 @@ def load_csv(filename: str) -> str:
     if filename.endswith(".csv"):
         return "my,super,csv"
     else:
-        raise FileNotFoundError(f"${filename} is not a csv file")
+        raise FileNotFoundError(f"{filename} is not a csv file")
