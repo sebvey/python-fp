ad367cf9b4752b5adbd3e75aaf5371a2c3d0b2ad
jeliermann <jliermann@ippon.fr>
2025-02-05 16:49:57 +0100

    improve: test xresult mypy compatible

--- src/xfp/xresult/__init__.py
+++ src/xfp/xresult/__init__.py
@@ -1,4 +1,10 @@
-from xfp.xresult._xresult import Xresult, XRBranch, XresultError
+from xfp.xresult._xresult import (
+    Xresult,
+    XRBranch,
+    XresultError,
+    XresultLikeR,
+    XresultLikeL,
+)
 from xfp.xresult.xeither import Xeither
 from xfp.xresult.xoption import Xopt
 from xfp.xresult.xtry import Xtry
