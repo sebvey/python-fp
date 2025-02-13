30f3953e532c01d8c963aaf384a357bbd18fa30d
jliermann <liermannjerome@gmail.com>
2025-02-13 16:41:15 +0100

    improve: mypy xeither, xtry, xlist constructor

--- tests/xresult/test_xtry.py
+++ tests/xresult/test_xtry.py
@@ -1,17 +1,18 @@
+from typing import Never
 from xfp import Xtry, Xresult, XRBranch
 
 
-def test_from_unsafe_lift_success():
+def test_from_unsafe_lift_success() -> None:
     def input():
         return 3
 
-    expected = Xtry.Success(3)
+    expected = Xtry.Success[int](3)
     actual = Xtry.from_unsafe(input)
 
     assert actual == expected
 
 
-def test_from_unsafe_lift_failure():
+def test_from_unsafe_lift_failure() -> None:
     def input():
         raise Exception("fail")
 
@@ -20,17 +21,17 @@ def test_from_unsafe_lift_failure():
     assert isinstance(actual.value, Exception)
 
 
-def test_safed_lift_success():
+def test_safed_lift_success() -> None:
     @Xtry.safed
     def actual():
         return 3
 
-    expected = Xtry.Success(3)
+    expected = Xtry.Success[int](3)
 
     assert actual() == expected
 
 
-def test_safed_lift_failure():
+def test_safed_lift_failure() -> None:
     @Xtry.safed
     def actual():
         raise Exception("fail")
@@ -38,34 +39,34 @@ def test_safed_lift_failure():
     assert isinstance(actual().value, Exception)
 
 
-def test_Xtry_instantiate_xresult():
-    success = Xtry.Success(3)
-    failure = Xtry.Failure(Exception("fail"))
+def test_Xtry_instantiate_xresult() -> None:
+    success = Xtry.Success[int](3)
+    failure = Xtry.Failure[Exception](Exception("fail"))
 
     assert isinstance(success, Xresult)
     assert isinstance(failure, Xresult)
-    assert success == Xresult(3, XRBranch.RIGHT)
+    assert success == Xresult[Never, int](3, XRBranch.RIGHT)
     assert failure.branch == XRBranch.LEFT
 
 
-def test_success_can_be_pattern_match():
-    match Xresult(3, XRBranch.RIGHT):
+def test_success_can_be_pattern_match() -> None:
+    match Xresult[Never, int](3, XRBranch.RIGHT):
         case Xtry.Success(v):
             assert v == 3
         case _:
             assert False
 
 
-def test_failure_can_be_pattern_match():
-    match Xresult(Exception("fail"), XRBranch.LEFT):
+def test_failure_can_be_pattern_match() -> None:
+    match Xresult[Exception, Never](Exception("fail"), XRBranch.LEFT):
         case Xtry.Failure(e):
             assert isinstance(e, Exception) and e.args[0] == "fail"
         case _:
             assert False
 
 
-def test_empty_do_not_false_positive():
-    match Xresult(None, XRBranch.LEFT):
+def test_empty_do_not_false_positive() -> None:
+    match Xresult[None, Never](None, XRBranch.LEFT):
         case Xtry.Failure(_):
             assert False
         case _:
