120d6a3ec16abc7af2c00928041748f695fd87da
jeliermann <jliermann@ippon.fr>
2025-02-17 10:40:56 +0100

    improve: make tests pass

--- src/xfp/utils.py
+++ src/xfp/utils.py
@@ -1,12 +1,13 @@
+from typing import Any
 import xfp.functions as f
 from deprecation import deprecated  # type: ignore
 
 # type: ignore
-deprecated("1.1.0", "2.0.0", "moved to xfp.functions")
-id = f.id
-deprecated("1.1.0", "2.0.0", "moved to xfp.functions")
-tupled = f.tupled
-deprecated("1.1.0", "2.0.0", "moved to xfp.functions")
-curry = f.curry
-deprecated("1.1.0", "2.0.0", "moved to xfp.functions")
-curry_method = f.curry_method
+deprecated("1.1.0", "2.0.0", details="moved to xfp.functions")
+id: f.F1[[Any], Any] = f.id
+deprecated("1.1.0", "2.0.0", details="moved to xfp.functions")
+tupled: f.F2[[f.F1[..., Any]], [tuple], Any] = f.tupled
+deprecated("1.1.0", "2.0.0", details="moved to xfp.functions")
+curry: f.F2[[f.F1[..., Any]], ..., Any] = f.curry
+deprecated("1.1.0", "2.0.0", details="moved to xfp.functions")
+curry_method: f.F2[[f.F1[..., Any]], ..., Any] = f.curry_method
